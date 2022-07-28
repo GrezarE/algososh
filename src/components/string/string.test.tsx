e3a4150c24d3aaa0d415da1f9216072b25c4e17a
Grechishnikov-Zarin Evgeniy <grecha-zarin@yandex.ru>
2022-07-28 22:13:03 +0300

    string & sort async test ready

--- src/components/string/string.test.tsx
+++ src/components/string/string.test.tsx
@@ -1,58 +1,40 @@
 import React from "react";
-import {
-  render,
-  screen,
-  fireEvent,
-  waitFor,
-  act,
-} from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { StringPage } from "./string";
-import { ElementStates } from "../../types/element-states";
 
-// describe("Алгоритм разворота строки", () => {
-//   const stringTest = (
-//     valueString: string,
-//     resultTest: string | undefined,
-//     test: string
-//   ) => {
-//     it(`Test ${test}`, async () => {
-//       jest.useFakeTimers();
-//       render(<StringPage />);
+describe("Алгоритм разворота строки", () => {
+  const stringTest = (
+    valueString: string,
+    resultTestFirst: string | undefined,
+    resultTestSecond: string | undefined,
+    test: string
+  ) => {
+    it(`Test ${test}`, async () => {
+      jest.useFakeTimers();
+      render(<StringPage />);
 
-//       const button: HTMLButtonElement = screen.getByTestId("button");
-//       const input: HTMLInputElement = screen.getByTestId("input");
-//       fireEvent.change(input, { target: { value: valueString } });
+      const button: HTMLButtonElement = screen.getByTestId("button");
+      const input: HTMLInputElement = screen.getByTestId("input");
+      fireEvent.change(input, { target: { value: valueString } });
 
-//       fireEvent.click(button);
+      fireEvent.click(button);
 
-//       jest.advanceTimersByTime(5000);
+      jest.advanceTimersByTime(5000);
 
-//       expect(screen?.queryByTestId("Circle_0")?.textContent).toBe(resultTest);
-//     });
-//   };
-//   stringTest("12345", "5", "with odd");
-//   stringTest("123456", "6", "with even");
-//   stringTest("1", "1", "with one");
-//   stringTest("", undefined, "with zero");
-// });
+      const circle = screen.queryByTestId("Circle_0");
+      const circle1 = screen.queryByTestId("Circle_1");
 
-it("Test", async () => {
-  jest.useFakeTimers();
-  render(<StringPage />);
-  const button: HTMLButtonElement = screen.getByTestId("button");
-  const input: HTMLInputElement = screen.getByTestId("input");
-  fireEvent.change(input, { target: { value: "1234567" } });
+      await waitFor(() => {
+        expect(circle?.textContent).toBe(resultTestFirst);
+      });
 
-  fireEvent.click(button);
-
-  jest.advanceTimersByTime(5000);
-  const circle = screen.queryByTestId("Circle_0");
-  const circle1 = screen.queryByTestId("Circle_4");
-
-  await waitFor(() => {
-    expect(circle?.textContent).toBe("7");
-  });
-  await waitFor(() => {
-    expect(circle1?.textContent).toBe("3");
-  });
+      await waitFor(() => {
+        expect(circle1?.textContent).toBe(resultTestSecond);
+      });
+    });
+  };
+  stringTest("12345", "5", "4", "with odd");
+  stringTest("1234", "4", "3", "with even");
+  stringTest("1", "1", undefined, "with one");
+  stringTest("", undefined, undefined, "with zero");
 });
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -9,7 +9,6 @@ import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 import { ElementStates } from "../../types/element-states";
-import { DELAY_IN_MS } from "../../constants/delays";
 import styles from "./string.module.css";
 
 interface ICircle {
@@ -23,36 +22,33 @@ export const StringPage = () => {
   const [isLoader, setIsLoader] = useState<boolean>(false);
   const [buttonDisabled, setButtonDisabled] = useState(false);
 
-  const stringReverse = (text: string) => {
+  const stringReverse = async (text: string) => {
     let obj = text.split("").map((item) => {
       return { color: ElementStates.Default, letter: item };
     });
     setResult([...obj]);
     for (let i = 0; i < obj.length / 2; i++) {
-      (function () {
+      obj[i] = { ...obj[i], color: ElementStates.Changing };
+      obj[obj.length - 1 - i] = {
+        ...obj[obj.length - 1 - i],
+        color: ElementStates.Changing,
+      };
+      setResult([...obj]);
+      await new Promise((resolve: any) =>
         setTimeout(() => {
-          obj[i] = { ...obj[i], color: ElementStates.Changing };
-          obj[obj.length - 1 - i] = {
-            ...obj[obj.length - 1 - i],
-            color: ElementStates.Changing,
-          };
-          setResult([...obj]);
-          setTimeout(() => {
-            const first = obj[i].letter;
-            const second = obj[obj.length - 1 - i].letter;
-            obj[i] = { letter: second, color: ElementStates.Modified };
-            obj[obj.length - 1 - i] = {
-              letter: first,
-              color: ElementStates.Modified,
-            };
-            setResult([...obj]);
-          }, DELAY_IN_MS);
-        }, DELAY_IN_MS * i);
-      })();
+          resolve();
+        }, 500)
+      );
+      const first = obj[i].letter;
+      const second = obj[obj.length - 1 - i].letter;
+      obj[i] = { letter: second, color: ElementStates.Modified };
+      obj[obj.length - 1 - i] = {
+        letter: first,
+        color: ElementStates.Modified,
+      };
+      setResult([...obj]);
     }
-    setTimeout(() => {
-      setIsLoader(false);
-    }, DELAY_IN_MS * (obj.length / 2));
+    setIsLoader(false);
   };
 
   const onChange: ChangeEventHandler<HTMLInputElement> = (evt) => {
@@ -62,32 +58,6 @@ export const StringPage = () => {
     evt.preventDefault();
     setIsLoader(true);
     stringReverse(inputText);
-    // let obj = inputText.split("").map((item) => {
-    //   return { color: ElementStates.Default, letter: item };
-    // });
-    // setResult([...obj]);
-    // for (let i = 0; i < obj.length / 2; i++) {
-    //   obj[i] = { ...obj[i], color: ElementStates.Changing };
-    //   obj[obj.length - 1 - i] = {
-    //     ...obj[obj.length - 1 - i],
-    //     color: ElementStates.Changing,
-    //   };
-    //   setResult([...obj]);
-    //   await new Promise((resolve: any) =>
-    //     setTimeout(() => {
-    //       resolve();
-    //     }, DELAY_IN_MS)
-    //   );
-    //   const first = obj[i].letter;
-    //   const second = obj[obj.length - 1 - i].letter;
-    //   obj[i] = { letter: second, color: ElementStates.Modified };
-    //   obj[obj.length - 1 - i] = {
-    //     letter: first,
-    //     color: ElementStates.Modified,
-    //   };
-    //   setResult([...obj]);
-    // }
-    // setIsLoader(false);
   };
 
   useEffect(() => {
@@ -98,9 +68,11 @@ export const StringPage = () => {
     }
   }, [inputText]);
 
-  // useEffect(() => {
-  //   console.log(result);
-  // }, [result]);
+  useEffect(() => {
+    return () => {
+      setResult(undefined);
+    };
+  }, []);
 
   return (
     <>
@@ -135,104 +107,9 @@ export const StringPage = () => {
 };
 
 export const StringComponent: React.FC = () => {
-  // const [inputText, changeInputText] = useState("");
-  // const [result, setResult] = useState<ICircle[]>();
-  // const [isLoader, setIsLoader] = useState<boolean>(false);
-  // const [buttonDisabled, setButtonDisabled] = useState(false);
-
-  // const stringReverse = (text: string) => {
-  //   let obj = text.split("").map((item) => {
-  //     return { color: ElementStates.Default, letter: item };
-  //   });
-  //   setResult([...obj]);
-  //   for (let i = 0; i < obj.length / 2; i++) {
-  //     (function () {
-  //       setTimeout(() => {
-  //         obj[i] = { ...obj[i], color: ElementStates.Changing };
-  //         obj[obj.length - 1 - i] = {
-  //           ...obj[obj.length - 1 - i],
-  //           color: ElementStates.Changing,
-  //         };
-  //         setResult([...obj]);
-  //         setTimeout(() => {
-  //           const first = obj[i].letter;
-  //           const second = obj[obj.length - 1 - i].letter;
-  //           obj[i] = { letter: second, color: ElementStates.Modified };
-  //           obj[obj.length - 1 - i] = {
-  //             letter: first,
-  //             color: ElementStates.Modified,
-  //           };
-  //           setResult([...obj]);
-  //         }, DELAY_IN_MS);
-  //       }, DELAY_IN_MS * i);
-  //     })();
-  //   }
-  //   setTimeout(() => {
-  //     setIsLoader(false);
-  //   }, DELAY_IN_MS * (obj.length / 2));
-  // };
-
-  // const onChange: ChangeEventHandler<HTMLInputElement> = (evt) => {
-  //   changeInputText(evt.target.value);
-  // };
-  // const onSubmit: FormEventHandler<HTMLFormElement> = async (evt) => {
-  //   evt.preventDefault();
-  //   setIsLoader(true);
-  //   // stringReverse(inputText);
-  //   let obj = inputText.split("").map((item) => {
-  //     return { color: ElementStates.Default, letter: item };
-  //   });
-  //   setResult([...obj]);
-  //   for (let i = 0; i < obj.length / 2; i++) {
-  //     obj[i] = { ...obj[i], color: ElementStates.Changing };
-  //     obj[obj.length - 1 - i] = {
-  //       ...obj[obj.length - 1 - i],
-  //       color: ElementStates.Changing,
-  //     };
-  //     setResult([...obj]);
-  //     await new Promise((resolve: any) =>
-  //       setTimeout(() => {
-  //         resolve();
-  //       }, DELAY_IN_MS)
-  //     );
-  //     const first = obj[i].letter;
-  //     const second = obj[obj.length - 1 - i].letter;
-  //     obj[i] = { letter: second, color: ElementStates.Modified };
-  //     obj[obj.length - 1 - i] = {
-  //       letter: first,
-  //       color: ElementStates.Modified,
-  //     };
-  //     setResult([...obj]);
-  //   }
-  //   setIsLoader(false);
-  // };
-
-  // useEffect(() => {
-  //   if (inputText.length < 1) {
-  //     setButtonDisabled(true);
-  //   } else {
-  //     setButtonDisabled(false);
-  //   }
-  // }, [inputText]);
-
   return (
     <SolutionLayout title="Строка">
       <StringPage />
-      {/* <form className={styles.form__box} onSubmit={onSubmit}>
-        <Input maxLength={11} isLimitText={true} onChange={onChange} />
-        <Button
-          text="Развернуть"
-          type="submit"
-          isLoader={isLoader}
-          disabled={buttonDisabled}
-        />
-      </form>
-      <div className={styles.circles__box}>
-        {result &&
-          result.map((item: ICircle, index: number) => (
-            <Circle letter={item.letter} state={item.color} key={index} />
-          ))}
-      </div> */}
     </SolutionLayout>
   );
 };
